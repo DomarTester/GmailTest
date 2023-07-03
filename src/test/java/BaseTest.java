f1a5e4a134866d3bcfe283905e78dd0aa1d743de
Łukasz Domaracki <lk.domar@gmail.com>
2023-07-03 08:18:38 +0200

    no static

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,9 +4,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
 
 public class BaseTest {
-    protected static WebDriver driver;
+    protected WebDriver driver;
     @BeforeTest
-    public static void openBrowser() {
+    public void openBrowser() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.get("https://www.google.com/intl/pl/gmail/about/");
--- src/test/java/GmailTest.java
+++ src/test/java/GmailTest.java
@@ -4,7 +4,7 @@ import org.testng.annotations.Test;
 public class GmailTest extends BaseTest{
 
     @Test
-    public static void happyPath (){
+    public void happyPath (){
         driver.get("https://www.google.com/intl/pl/gmail/about/");
         driver.findElement(By.cssSelector("[data-action='sign in']")).click();
         driver.findElement(By.id("identifierId")).click();
